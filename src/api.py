cb2b7f10392817d885f3b619f84aa0d32766f811
GabrielMenezes <dmenezes.gabriel@gmail.com>
2020-01-15 22:56:18 -0300

    Add routes
    Add routes:
    - To load all activities
    - To load today activities
    - To load an activity range

--- src/api.py
+++ src/api.py
@@ -1,5 +1,13 @@
 from src.controllers.activity import DefaultActivity as activity_controller
 
 
-async def load_activities():
-    return activity_controller.load_activities()
+async def load_all():
+    return activity_controller.load_all()
+
+
+async def load_today():
+    return activity_controller.load_today()
+
+
+async def load_range(start_date, end_date):
+    return activity_controller.load_range(start_date, end_date)
--- src/sanic_api.py
+++ src/sanic_api.py
@@ -1,4 +1,5 @@
 import asyncio
+from datetime import datetime
 import logging
 from sanic import Sanic
 from sanic.response import json
@@ -11,10 +12,26 @@ _logger = logging.getLogger("Sanic")
 app = Sanic(__name__)
 
 
-@app.route("/load", methods=["GET"])
-async def load_activities(request):
-    activities_list = await api.load_activities()
+@app.route("/load-all", methods=["GET"])
+async def load_all(request):
+    activities_list = await api.load_all()
     return json(activities_list)
+
+
+@app.route("/load-today", methods=["GET"])
+async def load_today(request):
+    today_activities_list = await api.load_today()
+    return json(today_activities_list)
+
+
+@app.route(
+    "/load-range/<selected_start_date>/<selected_end_date>",
+    methods=["GET", "POST"])
+async def load_today(request, selected_start_date, selected_end_date):
+    start_date = datetime.strptime(selected_start_date, "%Y-%m-%d").date()
+    end_date = datetime.strptime(selected_end_date, "%Y-%m-%d").date()
+    today_activities_list = await api.load_range(start_date, end_date)
+    return json(today_activities_list)
 
 
 @app.route("/", methods=["GET"])
@@ -26,5 +43,5 @@ def start():
     _logger.info("Starting")
     loop = asyncio.get_event_loop()
     server = app.create_server(
-        host="0.0.0.0", port=8080, return_asyncio_server=True)
+        host="0.0.0.0", port=8081, return_asyncio_server=True)
     loop.create_task(server)
