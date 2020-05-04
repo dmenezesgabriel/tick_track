88b688baa5758cff23b2a0fe998463595502c62c
Gabriel Dias Menezes <dmenezes.gabriel@gmail.com>
2020-05-03 22:42:32 -0300

    Add front-end
    Add front-end.

---
+++ frontend/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+// Tree of elements in html
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Render the app at the screen inside the root div on index.hmtl
+ReactDOM.render(<App />, document.getElementById('root'));
