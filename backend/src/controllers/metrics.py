d6bcba7283c79770615b59562fc1ee3b4235079a
Gabriel Dias Menezes <dmenezes.gabriel@gmail.com>
2020-06-04 20:08:40 -0300

    Change prepare names from activity to metrics

--- backend/src/controllers/metrics.py
+++ backend/src/controllers/metrics.py
@@ -1,7 +1,23 @@
 import pandas as pd
 
 
-def prepare_df(df, column):
+def make_descriptions(df):
+    df['split_name'] = df['name'].apply(lambda x: x.split('-'))
+    df['main_description'] = df['split_name'].apply(lambda x: x[-1])
+    df['detailed_description'] = (
+        df['split_name'].apply(
+            lambda x: ','.join(x[1:-1])
+            if len(x) > 2 else (
+                 ','.join(x[0:-1]) if len(x) > 1 else None
+            )
+        )
+    )
+    df['more_details'] = (
+        df['split_name'].apply(lambda x: x[0] if len(x) > 2 else None))
+    return df
+
+
+def make_chart_data(df, column):
     df_grouped = (
         df
         .groupby([column])
@@ -10,9 +26,9 @@ def prepare_df(df, column):
         .sort_values(by=['duration'], ascending=False)
     )
 
-    df_grouped['duration'] = (
-        df_grouped['duration'].apply(lambda x: round(x, 2))
-    )
+    # df_grouped['duration'] = (
+    #     df_grouped['duration'].apply(lambda x: round(x, 2))
+    # )
 
     df_grouped['time'] = (
         df_grouped['duration']
@@ -39,8 +55,9 @@ def total_idle(df):
 
 def metrics_bundle(data, drill_level):
     df = pd.DataFrame(data)
+    df_detail = make_descriptions(df)
     return dict(
-        total_duration=total_duration(df),
-        total_idle=total_idle(df),
-        table_data=prepare_df(df, drill_level)
+        total_duration=total_duration(df_detail),
+        total_idle=total_idle(df_detail),
+        table_data=make_chart_data(df_detail, drill_level)
     )
